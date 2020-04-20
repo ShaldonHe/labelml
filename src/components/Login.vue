783f03434507a1ad758a0a21d5899069965cd5fe
Shendon He <seariver@foxmail.com>
2020-04-20 23:32:11 +0800

    .

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,44 +1,17 @@
 <template>
   <v-app id="inspire" align="center" justify="center">
-    <v-content >
-      <v-container
-        class="fill-height"
-        fluid
-        dark
-      >
-        <v-row
-          align-center
-          justify-center
-        >
-          <v-col
-            cols="12"
-            sm="8"
-            md="4"
-          >
+    <v-content>
+      <v-container class="fill-height" fluid dark>
+        <v-row align-center justify-center>
+          <v-col cols="12" sm="8" md="4">
             <v-card class="elevation-12">
-              <v-toolbar
-                color="primary"
-                dark
-                flat
-              >
+              <v-toolbar color="primary" dark flat>
                 <v-toolbar-title>登陆</v-toolbar-title>
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field
-                    label="登陆"
-                    name="login"
-                    prepend-icon="person"
-                    type="text"
-                  />
-
-                  <v-text-field
-                    id="password"
-                    label="密码"
-                    name="password"
-                    prepend-icon="lock"
-                    type="password"
-                  />
+                  <v-text-field label="登陆" name="login" prepend-icon="person" type="text" />
+                  <v-text-field id="password" label="密码" name="password" prepend-icon="lock" type="password" />
                 </v-form>
               </v-card-text>
               <v-card-actions>
@@ -54,10 +27,10 @@
 </template>
 
 <script>
-  export default {
-    name: "login",
-    props: {
-      source: String,
-    },
+export default {
+  name: "login",
+  props: {
+    source: String
   }
+};
 </script>
