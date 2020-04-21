0d21a4cdb32ebe3b9e276df8d69e315346b7c2bd
Sheldon.He <seariver@foxmail.com>
2020-04-21 15:22:24 +0800

    .

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,58 +1,70 @@
-<!--<template>
-  <v-app id="inspire" align="center" justify="center">
+<template>
+  
+<div id="app">
+  <v-app id="inspire">
     <v-content>
-      <v-container class="fill-height" fluid dark>
-        <v-row align-center justify-center>
-          <v-col cols="12" sm="8" md="4">
+      <v-container
+        fluid
+        fill-height
+      >
+        <v-layout
+          align-center
+          justify-center
+        >
+          <v-flex
+            xs12
+            sm8
+            md4
+          >
             <v-card class="elevation-12">
-              <v-toolbar color="primary" dark flat>
-                <v-toolbar-title>登陆</v-toolbar-title>
+              <v-toolbar
+                color="primary"
+                dark
+                flat
+              >
+                <v-toolbar-title class="text-center">登陆</v-toolbar-title>
+                <v-spacer></v-spacer>
+                
               </v-toolbar>
               <v-card-text>
                 <v-form>
-                  <v-text-field label="登陆" name="login" prepend-icon="person" type="text" />
-                  <v-text-field id="password" label="密码" name="password" prepend-icon="lock" type="password" />
+                  <v-text-field
+                    label="登陆"
+                    name="login"
+                    prepend-icon="person"
+                    type="text"
+                  ></v-text-field>
+
+                  <v-text-field
+                    id="password"
+                    label="密码"
+                    name="password"
+                    prepend-icon="lock"
+                    type="password"
+                  ></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
-                <v-spacer />
+                <v-spacer></v-spacer>
                 <v-btn color="primary">登陆</v-btn>
               </v-card-actions>
             </v-card>
-          </v-col>
-        </v-row>
+          </v-flex>
+        </v-layout>
       </v-container>
     </v-content>
   </v-app>
-</template>-->
-
-<template>
-<div style="width:100%; align:center;">
-<v-card class="d-flex align-center mb-6`" color="grey lighten-2" flat height="100" style="width:400px; margin: auto; top: 200px;" tile>
-  <v-card class="pa-2" outlined tile >
-    <v-toolbar color="primary" dark flat>
-      <v-toolbar-title align="center">登陆</v-toolbar-title>
-    </v-toolbar>
-    <v-card-text>
-      <v-form>
-        <v-text-field label="登陆" name="login" prepend-icon="person" type="text" />
-        <v-text-field id="password" label="密码" name="password" prepend-icon="lock" type="password" />
-      </v-form>
-    </v-card-text>
-    <v-card-actions>
-      <v-spacer />
-      <v-btn color="primary">登陆</v-btn>
-    </v-card-actions>
-  </v-card>
-</v-card>
 </div>
-</template>
 
+</template>
 <script>
+
 export default {
-  name: "login",
-  props: {
-    source: String
-  }
-};
+  name:'login',
+  data () {
+    return {
+      drawer: null
+      }
+    }
+}
 </script>
