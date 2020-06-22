a3004b80fdd9758ef3a6b78a787cf2a74df69a1d
Shendon He <seariver@foxmail.com>
2020-06-23 03:58:38 +0800

    .

--- go_server/main.go
+++ go_server/main.go
@@ -13,10 +13,14 @@ func root(c *gin.Context) {
 func login(c *gin.Context) {
 	c.String(http.StatusOK, "sessiion:'adasdasdasd'")
 }
+func image(c *gin.Context) {
+	c.File("/home/sheldon/Document/code/beyes/labelml/server/project/skin-feature/skin.jpg")
+}
 
 func main() {
 	router := gin.Default()
 	router.GET("/", root)
 	router.GET("/login", login)
+	router.GET("/image", image)
 	router.Run(":8000")
 }
