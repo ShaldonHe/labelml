5e2ed9391577f4a241cc132c2fe6ecba8181af72
Sheldon.He <seariver@foxmail.com>
2020-06-24 10:43:39 +0800

    .

--- go_server/datacenter/main.go
+++ go_server/datacenter/main.go
@@ -7,20 +7,26 @@ import (
 )
 
 func root(c *gin.Context) {
-	c.String(http.StatusOK, "Hello World!")
+	c.String(http.StatusOK, "DataCenter Interface!")
 }
 
-func login(c *gin.Context) {
-	c.String(http.StatusOK, "sessiion:'adasdasdasd'")
-}
 func image(c *gin.Context) {
+	c.File("/home/sheldon/Document/code/beyes/labelml/server/project/skin-feature/skin.jpg")
+}
+
+func wsi(c *gin.Context) {
+	c.File("/home/sheldon/Document/code/beyes/labelml/server/project/skin-feature/skin.jpg")
+}
+
+func thumbnail(c *gin.Context) {
 	c.File("/home/sheldon/Document/code/beyes/labelml/server/project/skin-feature/skin.jpg")
 }
 
 func main() {
 	router := gin.Default()
 	router.GET("/", root)
-	router.GET("/login", login)
 	router.GET("/image", image)
+	router.GET("/wsi", wsi)
+	router.GET("/thumbnail", thumbnail)
 	router.Run(":8000")
 }
